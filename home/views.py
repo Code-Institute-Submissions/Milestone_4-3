f65c4b1f1807a8eb66886e22d23e3f20af416dbf
CojanRadu <raduacojan@gmail.com>
2020-11-16 10:42:32 +0000

    improved home page

--- home/views.py
+++ home/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from django.contrib import messages
+from properties.models import Property
 
 def index(request):
     """ view that returns the index page """
-    return render(request, 'home/index.html')
+
+    last_six = Property.objects.order_by('created')[:6]
+    last_six_in_ascending_order = reversed(last_six)
+
+    carousel_items = Property.objects.order_by('rating')[:5]
+
+    context = {
+        'properties': last_six_in_ascending_order,
+        'carousel_items': carousel_items,
+    }
+
+    return render(request, 'home/index.html', context)
--- properties/models.py
+++ properties/models.py
@@ -45,6 +45,7 @@ class Property(models.Model):
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image_name = models.ImageField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.short_description
