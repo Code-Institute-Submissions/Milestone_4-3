5447ef76da93bed756c464b1beced69d2f4ead20
CojanRadu <raduacojan@gmail.com>
2020-11-28 13:44:43 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
-
+import stripe
 
 def checkout(request):
+
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     booking = request.session.get('book_ses', {})
     if not booking:
         messages.error(request, "You dont have any bookings yet")
@@ -14,6 +19,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': stripe_public_key,
+        'client_secret': stripe_secret_key,
     }
 
     return render(request, template, context)
