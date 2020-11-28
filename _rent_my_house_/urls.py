ad2cdb50d98d796a46aaaf486f71b50dc32a48e9
CojanRadu <raduacojan@gmail.com>
2020-11-28 11:19:52 +0000

    added checkout view

--- _rent_my_house_/urls.py
+++ _rent_my_house_/urls.py
@@ -25,4 +25,5 @@ urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + [
     path('profiles/', include('profiles.urls')),
     path('properties/', include('properties.urls')),
     path('booking/', include('booking.urls')),
+    path('checkout/', include('checkout.urls')),
 ]
--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
-# Create your views here.
+from .forms import OrderForm
+
+
+def checkout(request):
+    booking = request.session.get('book_ses', {})
+    if not booking:
+        messages.error(request, "You dont have any bookings yet")
+        return redirect(reverse('listings'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
