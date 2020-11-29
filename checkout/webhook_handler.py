6535a4a02e85299d65bab13ad77167fcd79663dd
CojanRadu <raduacojan@gmail.com>
2020-11-29 12:02:56 +0000

    added checkout form data caching

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,5 +1,11 @@
 from django.http import HttpResponse
 
+from .models import Order, OrderLineItem
+from properties.models import Property
+
+import json
+import time
+import datetime
 
 class StripeWH_Handler:
     """Handle Stripe webhooks"""
@@ -13,3 +19,93 @@ class StripeWH_Handler:
         """
         return HttpResponse(
             content=f'Webhook received: {event["type"]}', status=200)
+
+    def handle_payment_intent_succeeded(self, event):
+        """
+        Handle the payment_intent.succeeded webhook from Stripe
+        """
+        intent = event.data.object
+        pid = intent.id
+        booking = intent.metadata.booking
+        save_info = intent.metadata.save_info
+
+        # CHECK HEREEEEE
+        billing_details = intent.charges.data[0].billing_details
+        total = round(intent.charges.data[0].amount / 100, 2)
+
+        order_exists = False
+        attempt = 1
+        while attempt <= 5:
+            try:
+                order = Order.objects.get(
+                    full_name__iexact=billing_details.name,
+                    email__iexact=billing_details.email,
+                    phone_number__iexact=billing_details.phone,
+                    country__iexact=billing_details.address.country,
+                    postcode__iexact=billing_details.address.postal_code,
+                    town_or_city__iexact=billing_details.address.city,
+                    street_address1__iexact=billing_details.address.line1,
+                    street_address2__iexact=billing_details.address.line2,
+                    county__iexact=billing_details.address.state,
+                    total=total,
+                    original_booking=booking,
+                    stripe_pid=pid,
+                )
+                order_exists = True
+                break
+            except Order.DoesNotExist:
+                attempt += 1
+                time.sleep(1)
+        if order_exists:
+            return HttpResponse(
+                content=f'Webhook received: {event["type"]} | SUCCESS: Verified order already in database',
+                status=200)
+        else:
+            order = None
+            try:
+                order = Order.objects.create(
+                    full_name=billing_details.name,
+                    email=billing_details.email,
+                    phone_number=billing_details.phone,
+                    country=billing_details.address.country,
+                    postcode=billing_details.address.postal_code,
+                    town_or_city=billing_details.address.city,
+                    street_address1=billing_details.address.line1,
+                    street_address2=billing_details.address.line2,
+                    county=billing_details.address.state,
+                    original_booking=booking,
+                    stripe_pid=pid,
+                )
+                # FIXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+                # for item_id, item_data in json.loads(booking).items():
+                for i in range(len(json.loads(booking))):
+                    one_book = json.loads(booking)[i]
+                    delta = datetime.datetime.strptime(one_book['check_out'], '%Y-%m-%d') - datetime.datetime.strptime(one_book['check_in'], '%Y-%m-%d')
+                    days = delta.days + 1
+
+                    property = Property.objects.get(id=one_book['property'])
+                    order_line_item = OrderLineItem(
+                        order=order,
+                        property=property,
+                        check_in=one_book['check_in'],
+                        check_out=one_book['check_out'],
+                        days=days,
+                    )
+                    order_line_item.save()
+            except Exception as e:
+                if order:
+                    order.delete()
+                return HttpResponse(
+                    content=f'Webhook received: {event["type"]} | ERROR: {e}',
+                    status=500)
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]} | SUCCESS: Created order in webhook',
+            status=200)
+
+    def handle_payment_intent_payment_failed(self, event):
+        """
+        Handle the payment_intent.payment_failed webhook from Stripe
+        """
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}', status=200)
+
