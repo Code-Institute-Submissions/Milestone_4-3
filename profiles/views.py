8cb1729093774ad5377c3bf5a55bf01f80f16a2b
CojanRadu <raduacojan@gmail.com>
2020-11-30 22:58:54 +0000

    added order history

--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-
+from checkout.models import Order
 
 from .models import UserProfile
 from .forms import UserProfileForm
@@ -25,6 +25,22 @@ def profile(request):
         'form': form,
         'on_profile_page': True,
         'orders': orders
+    }
+
+    return render(request, template, context)
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        f'This is a past confirmation for order number {order_number}. '
+        'A confirmation email was sent on the order date.'
+    ))
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
     }
 
     return render(request, template, context)
